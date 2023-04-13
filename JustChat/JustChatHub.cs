75e67c12c45c590d7c4670db4de4270904127cac
Łukasz Wąś <ljwasiu@gmail.com>
2023-04-13 19:50:35 +0200

    Initial commit.

---
+++ JustChat/JustChatHub.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace JustChat;
+
+public class JustChatHub : Hub
+{
+    public const string HubUrl = "/chat";
+
+    public async Task Broadcast(string username, string message)
+    {
+        await Clients.All.SendAsync("Broadcast", username, message);
+    }
+
+    public override Task OnConnectedAsync()
+    {
+        Console.WriteLine($"{Context.ConnectionId} joined the chat.");
+        return base.OnConnectedAsync();
+    }
+
+    public override async Task OnDisconnectedAsync(Exception e)
+    {
+        Console.WriteLine($"{Context.ConnectionId} left the chat. ({e?.Message})");
+        await base.OnDisconnectedAsync(e);
+    }
+}
